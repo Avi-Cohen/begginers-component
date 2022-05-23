7545ecb21e58cb939f85fdd8ac0ed1a5095f4f12
Avi Cohen <avic@insighting.io>
2022-05-23 20:51:47 +0300

    to download

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DemoComponent from "./components/DemoComponent/DemoComponent";
+import Sample from "./components/Sample/Sample";
 
 function App() {
+  const dataArr = [
+    {
+      paragraph: "12",
+      div: "12",
+      span: "12",
+      span2: "12",
+      h1: "3",
+    },
+    {
+      paragraph: "13",
+      div: "13",
+      span: "13",
+      span2: "13",
+      h1: "3",
+    },
+    {
+      paragraph: "14",
+      div: "14",
+      span: "14",
+      span2: "14",
+      h1: "3",
+    },
+    {
+      paragraph: "14",
+      div: "14",
+      span: "14",
+      span2: "14",
+      h1: "3",
+    },
+    {
+      paragraph: "15",
+      div: "16",
+      span: "16",
+      span2: "16",
+      h1: "3",
+    },
+  ];
+  const arr = ["Avi", "Ben", "Roy", "Ziv"];
+  const paragraph = "para";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DemoComponent />
+      <Sample paragraph={paragraph} div={"2"} span={"3"} span2={"4"} h1={"3"} /> 
+      {dataArr.map((item) => (
+        <Sample
+          paragraph={item.paragraph}
+          div={item.div}
+          span={item.span}
+          span2={item.span2}
+          h1={item.h1}
+        />
+      ))}
+      <br />
+      <div>HTML:</div>
+      {arr}
+    </>
   );
 }
 
